f4d5422c604ecaa50ca98e0f4a8d351187b1cdce
Vidya Sagar Gupta <v3sagar@gmail.com>
2020-10-05 01:08:24 +0530

    Module sb-payment-service added

--- sb-admin-dashboard/src/main/java/com/vidya/admin/dashboard/AdminDashboardApplication.java
+++ sb-admin-dashboard/src/main/java/com/vidya/admin/dashboard/AdminDashboardApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
+
 @EnableAdminServer
 @SpringBootApplication
 public class AdminDashboardApplication {
---
+++ sb-payment-service/README.md
@@ -0,0 +1,1 @@
+# sb-payment-service
