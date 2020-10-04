007506a46bdb0f6e0498587c9369b3ce5ca4819a
Vidya Sagar Gupta <v3sagar@gmail.com>
2020-10-05 00:03:40 +0530

    Module sb-admin-dashboard added

---
+++ sb-admin-dashboard/src/main/java/com/vidya/admin/dashboard/AdminDashboardApplication.java
@@ -0,0 +1,16 @@
+package com.vidya.admin.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDashboardApplication.class, args);
+	}
+
+}
---
+++ sb-admin-dashboard/src/test/java/com/vidya/admin/dashboard/AdminDashboardApplicationTest.java
@@ -0,0 +1,13 @@
+package com.vidya.admin.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDashboardApplicationTest {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
