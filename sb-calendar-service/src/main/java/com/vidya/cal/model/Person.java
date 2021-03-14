dcb9f78b95a0a0a488169b8d1266de7d3e2dc958
Vidya Sagar Gupta <v3sagar@gmail.com>
2021-03-14 16:51:12 +0530

    Project lombok added

--- sb-calendar-service/src/main/java/com/vidya/cal/model/Person.java
+++ sb-calendar-service/src/main/java/com/vidya/cal/model/Person.java
@@ -1,76 +1,24 @@
 package com.vidya.cal.model;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Builder
+@AllArgsConstructor
+@Data
 public class Person {
 
 	private String id;
 	private String name;
+	
+	@EqualsAndHashCode.Exclude
 	private String avatar; // www.gravatar.com/avatar/88c3d17245bc58e95d3ed6df1479108e
+	
+	@EqualsAndHashCode.Exclude
 	private String email;
+	
+	@EqualsAndHashCode.Exclude
 	private String phone;
-
-	public Person(String id, String name, String avatar, String email, String phone) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.avatar = avatar;
-		this.email = email;
-		this.phone = phone;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, name);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!(obj instanceof Person))
-			return false;
-		Person other = (Person) obj;
-		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAvatar() {
-		return avatar;
-	}
-
-	public void setAvatar(String avatar) {
-		this.avatar = avatar;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
 }
