cff409fc2070086b13cae03ae27f3d08c9ac9028
Vidya Sagar Gupta <v3sagar@gmail.com>
2025-09-13 21:45:38 +0530

    sb-auth-service module added

---
+++ sb-auth-service/src/main/java/com/example/sb_auth_service/SbAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAuthServiceApplication.class, args);
+	}
+
+}
